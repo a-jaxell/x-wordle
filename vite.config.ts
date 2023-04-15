623329405111cdc603008ef69fa1498d0d3ffc56
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-04-15 10:11:25 +0200

    updated config for dev env

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
+  build:{
+    outDir: "dist",
+  },
+  server: {
+    proxy: {
+
+      "/api": {
+        target: "http://localhost:5080/",
+        changeOrigin: true,
+        secure: false 
+      }
+    }
   }
 })
